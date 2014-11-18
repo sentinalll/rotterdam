e4a67887674b7a199da6e0029fe855a42c85a26e
parkhomenko <dmitriy.parkhomenko@icloud.com>
2014-11-18 19:55:29 +0200

    Add angular

--- js/showPasswd.js
+++
@@ -1,9 +0,0 @@
-$(".reveal").mousedown(function() {
-    $(".pwd").replaceWith($('.pwd').clone().attr('type', 'text'));
-})
-.mouseup(function() {
-	$(".pwd").replaceWith($('.pwd').clone().attr('type', 'password'));
-})
-.mouseout(function() {
-	$(".pwd").replaceWith($('.pwd').clone().attr('type', 'password'));
-});

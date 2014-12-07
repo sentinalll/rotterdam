0feb61c6067adc8c74a5269e20f56951d97e57b2
parkhomenko <dmitriy.parkhomenko@icloud.com>
2014-12-07 22:12:22 +0200

    add logout js

--- src/main/webapp/resources/js/userdata.js
+++ src/main/webapp/resources/js/userdata.js
@@ -5,20 +5,25 @@
                         url: "api/home",
                         datatype: "json",
                         contentType: "application/json; charset=utf-8",
-//                      success: function(data) {
                         statusCode: {
                         200: function (data) {
-//                        alert("ok");
-//                        alert(userinfo);
-//                        var name = userinfo.firstname;
-//                        var date = userinfo.date;
-//                        var userinfo = JSON.parse(userjson);
                         $("#username").append(data.firstname)
                         $("#userdate").append(data.date)
-//                        alert(data.firstname);
-//                        alert(data.date);
-
             }
                         }
                     });
+        $('#logout').click(function(){
+             $.ajax({
+              type: "POST",
+              url: "api/logout",
+              data: JSON.stringify(data),
+              contentType: "application/json; charset=utf-8",
+              dataType: "json",
+              statusCode: {
+                  200: function () {
+                      location.href='index.html';
+                  },
+              }
+          });
+        });
      });

f2191cb94eb2f0d04f7a26c4f381324bb5ddbd10
parkhomenko <dmitriy.parkhomenko@icloud.com>
2015-01-07 00:18:15 +0200

    new calendar add

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -8,4 +8,25 @@ app.controller('TimepickerDemoCtrl', function($scope, $http) {
   $scope.selectedTimeAsString = '10:00';
   $scope.sharedDate = new Date(new Date().setMinutes(0));
 });
+app.config(function($datepickerProvider) {
+  angular.extend($datepickerProvider.defaults, {
+    dateFormat: 'dd/MM/yyyy',
+    startWeek: 1
+  });
+})
 
+app.controller('DatepickerDemoCtrl', function($scope, $http) {
+
+  $scope.selectedDate = new Date();
+  $scope.selectedDateAsNumber = Date.UTC(1986, 1, 22);
+  // $scope.fromDate = new Date();
+  // $scope.untilDate = new Date();
+  $scope.getType = function(key) {
+    return Object.prototype.toString.call($scope[key]);
+  };
+
+  $scope.clearDates = function() {
+    $scope.selectedDate = null;
+  };
+
+});

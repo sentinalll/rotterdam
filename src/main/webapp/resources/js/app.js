deea6bf2ca2af8af7fb1f7b1d4c6c051e86ffef1
parkhomenko <dmitriy.parkhomenko@icloud.com>
2015-01-06 23:44:03 +0200

    Add angular selected time

---
+++ src/main/webapp/resources/js/app.js
@@ -0,0 +1,11 @@
+var app = angular.module('mgcrea.ngStrapDocs', ['ngAnimate', 'ngSanitize', 'mgcrea.ngStrap']);
+
+angular.module('mgcrea.ngStrapDocs')
+
+app.controller('TimepickerDemoCtrl', function($scope, $http) {
+  $scope.time = new Date(1970, 0, 1, 10, 30);
+  $scope.selectedTimeAsNumber = 10 * 36e5;
+  $scope.selectedTimeAsString = '10:00';
+  $scope.sharedDate = new Date(new Date().setMinutes(0));
+});
+
